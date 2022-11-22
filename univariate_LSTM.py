b004009c910d7d5ec5f1eac28595c8cda6b49294
Geoffrey Chang <gcha446@aucklanduni.ac.nz>
2022-11-22 15:51:26 +0800

    made start on LSTM model

--- univariate_LSTM.py
+++ univariate_LSTM.py
@@ -6,13 +6,15 @@ from keras.models import Sequential
 from sklearn.model_selection import KFold
 from helper_functions import *
 import numpy as np
+from keras import layers
+from tensorflow import keras
 
 folds = KFold(n_splits=10)
 
-def build_and_compile_model(norm):
+def build_and_compile_model():
     model = tf.keras.Sequential([
-        norm,
-        tf.keras.layers.LSTM(64, activation='relu'),
+        tf.keras.layers.LSTM(64, input_shape=(None, 12)),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(64),
         tf.keras.layers.Dense(1)
@@ -25,7 +27,7 @@ def build_and_compile_model(norm):
 
 if __name__ == "__main__":
     df = read_file_no(1)
-    normalizer = tf.keras.layers.Normalization(axis=-1)
+
     df = df.sample(frac=1, random_state=1)
     features, target = get_features_and_target(df)
     scores = []
@@ -35,8 +37,15 @@ if __name__ == "__main__":
                                            target.iloc[train_index], \
                                            target.iloc[test_index]
         tf.keras.backend.clear_session()
-        model_lstm = build_and_compile_model(normalizer)
-
+        model_lstm = tf.keras.Sequential()
+        model_lstm.add(layers.LSTM(64, input_shape=(12, 1)))
+        model_lstm.add(layers.BatchNormalization())
+        model_lstm.add(layers.Dense(1))
+        model_lstm.compile(
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            optimizer="adam",
+            metrics=["accuracy"],
+        )
         model_lstm.fit(x_train, y_train, epochs=100, verbose=1)
 
         y_hat = model_lstm.predict(y_test).flatten()

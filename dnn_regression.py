495d33273d5ca62f9749973e4d551b77628ce825
Geoffrey Chang <gcha446@aucklanduni.ac.nz>
2022-11-23 14:59:51 +0800

    changed to model all datasets

--- dnn_regression.py
+++ dnn_regression.py
@@ -12,9 +12,8 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 folds = KFold(n_splits=10)
 
 plt.style.use('ggplot')
-def build_and_compile_model(norm):
+def build_and_compile_model():
     model = keras.Sequential([
-        norm,
         keras.layers.Dense(64, activation='relu'),
         keras.layers.Dense(64),
         keras.layers.Dense(1)])
@@ -24,33 +23,36 @@ def build_and_compile_model(norm):
     return model
 
 if __name__ == "__main__":
-    normalizer = keras.layers.Normalization(axis=-1)
-    df = read_file_no(12)
-    best_features = find_best_features(df)
-    # df = df[df.Z >= 9.5]
-    df = df.sample(frac=1, random_state=1)
-    features, target = get_features_and_target(df)
+    # normalizer = keras.layers.Normalization()
+    files = read_all_files()
+    main_scores = []
+    for df in files:
+        df = normalize_df(df)
+        best_features = find_best_features(df)
+        # df = df[df.Z >= 9.5]
+        df = df.sample(frac=1, random_state=1)
+        features, target = get_features_and_target(df)
 
-    features = features[best_features]
-    scores = []
-    dnn_model = build_and_compile_model(normalizer)
-    for train_index, test_index in folds.split(features):
-        x_train, x_test, y_train, y_test = features.iloc[train_index], \
-                                               features.iloc[test_index], \
-                                           target.iloc[train_index], \
-                                           target.iloc[test_index]
-        history = dnn_model.fit(
-            x_train,
-            y_train,
-            verbose=0,
-            epochs=50)
+        features = features[best_features]
+        scores = []
+        dnn_model = build_and_compile_model()
+        for train_index, test_index in folds.split(features):
+            x_train, x_test, y_train, y_test = features.iloc[train_index], \
+                                                   features.iloc[test_index], \
+                                               target.iloc[train_index], \
+                                               target.iloc[test_index]
+            history = dnn_model.fit(
+                x_train,
+                y_train,
+                verbose=0,
+                epochs=50)
 
-        y_hat = dnn_model.predict(x_test).flatten()
-        scores.append(r2_score(y_test, y_hat))
-        z_plot_comparison(y_hat, y_test)
+            y_hat = dnn_model.predict(x_test).flatten()
+            scores.append(r2_score(y_test, y_hat))
+            z_plot_comparison(y_hat, y_test)
 
-    for s in scores:
-        print("R square: %.3f" % s)
-    print("Average R square: %.3f" % np.mean(scores))
-    print(best_features)
-
+        for s in scores:
+            print("R square: %.3f" % s)
+        main_scores.append(np.mean(scores))
+        print("Average R square: %.3f" % np.mean(scores))
+    print("MAIN Average R square: %.3f" % np.mean(main_scores))

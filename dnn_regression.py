bb5cb42c991ae1afe6487d388da4acb2cf6181a5
Geoffrey Chang <gcha446@aucklanduni.ac.nz>
2022-12-18 20:12:05 +0800

    implemented multiprocessing

--- dnn_regression.py
+++ dnn_regression.py
@@ -1,61 +1,89 @@
+import multiprocessing
+import warnings
+from itertools import repeat
+
 import numpy as np
-from matplotlib import pyplot as plt
-from sklearn.metrics import r2_score
 import tensorflow as tf
-from tensorflow import keras
+from keras.backend import clear_session
 from sklearn.model_selection import KFold
+from tensorflow import keras
+
 from helper_functions import *
 
-import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-folds = KFold(n_splits=6)
-
+n_pools = multiprocessing.cpu_count()
 plt.style.use('ggplot')
+
+
 def build_and_compile_model():
+    clear_session()
     model = keras.Sequential([
         keras.layers.Dense(64, activation='sigmoid'),
         keras.layers.Dense(64),
         keras.layers.Dense(1)])
 
     model.compile(loss='mean_absolute_error',
-                optimizer=keras.optimizers.Adam(0.001))
+                  metrics=[tf.keras.metrics.RootMeanSquaredError()],
+                  optimizer=tf.keras.optimizers.Adam(0.001))
     return model
 
+
+def run_cv_fold(feat, targ, train_index, test_index):
+    # split the data into training and testing sets for this fold
+    x_tra, x_val = feat.iloc[train_index], feat.iloc[test_index]
+    y_tra, y_val = targ.iloc[train_index], targ.iloc[test_index]
+
+    model_run = build_and_compile_model()
+
+    # evaluate the model on the test data
+    model_run.fit(x_tra, y_tra, epochs=50, batch_size=16, verbose=1)
+
+    Y_hat = model_run.predict(x_val)
+    return Y_hat, y_val
+
+
 if __name__ == "__main__":
-    # normalizer = keras.layers.Normalization()
-    files = read_all_files()
-    main_scores = []
+    separate = False
 
+    if separate:
+        df = read_all_files(1)
+        df_norm = normalize_df(df)
+        best_features = find_best_features(df_norm)
+        # df_norm = df_norm.sample(frac=1, random_state=1)
+        features, target = get_features_and_target(df_norm)
+        features = features[best_features]
+    else:
+        df = read_all_files()
+        df.pop(9)
+        df.pop(5)
+        df = pd.concat(df)
+        # df = df.sample(frac=1, random_state=12)
+        df = df.drop(["TIME", "S"], axis=1)
+        df_norm = normalize_df(df)
+        df_norm = df_norm.dropna()
+        features, target = df_norm.iloc[:, :-1], df_norm["Z"]
 
-    # for df in files:
-    df = read_all_files(1)
-    df = normalize_df(df)
-    best_features = find_best_features(df)
-    # df = df[df.Z >= 9.5]
-    df = df.sample(frac=1, random_state=1)
-    features, target = get_features_and_target(df)
-
-    features = features[best_features]
-    scores = []
-    dnn_model = build_and_compile_model()
+    kfold = KFold(n_splits=10, shuffle=True, random_state=42)
+    a = []
+    b = []
     predicted_overall = []
     real_overall = []
-    for train_index, test_index in folds.split(features):
-        x_train, x_test, y_train, y_test = features.iloc[train_index], \
-                                           features.iloc[test_index], \
-                                           target.iloc[train_index], \
-                                           target.iloc[test_index]
-        history = dnn_model.fit(
-            x_train,
-            y_train,
-            verbose=0,
-            epochs=50)
+    for train_ind, val_ind in kfold.split(features):
+        a.append(train_ind)
+        b.append(val_ind)
+        # hold_target.append(target1.iloc[val_ind])
 
-        y_hat = fit_model(dnn_model, x_train, y_train, x_test, y_test, epoc=10, name="DNN")
+    a, b = [train_ind for train_ind, _ in kfold.split(features)], [val_ind for _, val_ind in kfold.split(features)]
+    with multiprocessing.Pool(n_pools) as pool:
+        result = pool.starmap(run_cv_fold, zip(repeat(features), repeat(target), a, b))
 
-        predicted_overall.append(y_hat)
-        real_overall.append(y_test)
-        # z_plot(y_hat, y_test)
+    total_rmse = []
+    for predic, real in result:
+        predicted_overall.append(predic)
+        real_overall.append(real)
+        rmse_metric = tf.keras.metrics.RootMeanSquaredError()
+        rmse_metric.update_state(y_true=real, y_pred=predic)
+        total_rmse.append(rmse_metric.result().numpy())
 
-    z_plot(predicted_overall, real_overall)
-
+    print('Mean RMSE: %.3f' % np.mean(total_rmse))
+    z_plot(predicted_overall, real_overall, split=False)

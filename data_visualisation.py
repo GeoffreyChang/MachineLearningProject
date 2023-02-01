ecc1a6c546f7227f9a8c81fa9448bb203d63807f
Geoffrey Chang <gcha446@aucklanduni.ac.nz>
2023-02-01 11:32:41 +0800

    added 3x3 plot visual for data

--- data_visualisation.py
+++ data_visualisation.py
@@ -41,7 +41,7 @@ def boxplot_visual(df, title, save_fig):
     fig, axes = plt.subplots(1,2,figsize=(10,5))
     fig.tight_layout(pad=4)
     bp = features.boxplot(ax=axes[0])
-    bp.set_title(f'Boxplots for {title}', fontsize=15)
+    plt.title(f'Boxplots for {title}', fontsize=15)
     bp.set_xlabel("Features")
     bp.set_ylabel("Temperature")
 
@@ -77,7 +77,6 @@ def heatmap(df, title, save_fig):
 def plot_histogram(df):
     features, targets = get_features_and_target(df)
 
-    features = features.drop(["TIME", "S"], axis=1)
     for f in features:
         plt.figure()
         plt.title(f)
@@ -87,27 +86,85 @@ def plot_histogram(df):
     # plt.hist(features)
     # plt.show()
 
+def plot_data_3_by_3(n1, n2, n3, save_fig=False):
+    df1 = read_all_files(n1)
+    df2 = read_all_files(n2)
+    df3 = read_all_files(n3)
+
+    fig, axs = plt.subplots(3, 3, figsize=(15, 10))
+    fig.tight_layout(pad=2.65)
+    # df1
+    axs[0, 0].plot(df1["TIME"], df1["S"], 'k-')
+    axs[0, 0].set_title("Spindle Speed")
+
+    axs[1, 0].plot(df1["TIME"], df1.iloc[:, 2:12], '-')
+    axs[1, 0].set_title("Temperature")
+
+    axs[2, 0].plot(df1["TIME"], df1["Z"], 'k-')
+    axs[2, 0].set_title("Thermal Displacement")
+
+    axs[2, 0].set_xlabel(f"Time (min) \n [Dataset {n1}]")
+
+    # df2
+    axs[0, 1].plot(df2["TIME"], df2["S"], 'k-')
+    axs[0, 1].set_title("Spindle Speed")
+
+    axs[1, 1].plot(df2["TIME"], df2.iloc[:, 2:12], '-')
+    axs[1, 1].set_title("Temperature")
+
+    axs[2, 1].plot(df2["TIME"], df2["Z"], 'k-')
+    axs[2, 1].set_title("Thermal Displacement")
+
+    axs[2, 1].set_xlabel(f"Time (min) \n [Dataset {n2}]")
+
+    # df3
+    axs[0, 2].plot(df3["TIME"], df3["S"], 'k-')
+    axs[0, 2].set_title("Spindle Speed")
+
+    axs[1, 2].plot(df3["TIME"], df3.iloc[:, 2:12], '-')
+    axs[1, 2].set_title("Temperature")
+
+    axs[2, 2].plot(df3["TIME"], df3["Z"], 'k-')
+    axs[2, 2].set_title("Thermal Displacement")
+
+    axs[2, 2].set_xlabel(f"Time (min) \n [Dataset {n3}]")
+    # for ax in axs.flat:
+    #     ax.set(xlabel='Time (min)')
+
+    for ax in axs.flat:
+        ax.label_outer()
+
+
+    plt.show()
+
 if __name__ == "__main__":
-    # Plot style
-    plt.style.use('ggplot')
-    # to save as png
-    save_plots = False
-    # plot individual datasets or entire dataset
-    individual = True
-    # plot_histogram(read_file_no(2))
-    if individual:
-        files = read_all_files()
-        for k, data in enumerate(files):
-            tt = f"dataset_{k + 1}"
-            print(f"Best Features for {tt}: {find_best_features(data)}")
-            # data_visual(data, tt, save_plots)
-            boxplot_visual(data, tt, save_plots)
-            # heatmap(data, tt, save_plots)
-    else:
-        data = entire_dataset()
-        tt = "All Datasets Combined"
-        print(f"Best Features for {tt}: {find_best_features(data)}")
-        heatmap(data, tt, save_plots)
+    plot_data_3_by_3(13, 14, 1)
+    # # Plot style
+    # plt.style.use('ggplot')
+    # # to save as png
+    # save_plots = False
+    # # plot individual datasets or entire dataset
+    # individual = False
+    # # plot_histogram(read_file_no(2))
+    # if individual:
+    #     files = read_all_files()
+    #     for k, data in enumerate(files):
+    #         tt = f"dataset_{k + 1}"
+    #         print(f"Best Features for {tt}: {find_best_features(data)}")
+    #         # data_visual(data, tt, save_plots)
+    #         boxplot_visual(data, tt, save_plots)
+    #         # heatmap(data, tt, save_plots)
+    # else:
+    #     data = read_all_files()
+    #     data.pop(9) # remove the 10th file due to missing data
+    #     data.pop(5) # remove the 6th file due to extremely high values
+    #     data = pd.concat(data)
+    #     tt = "All Datasets Combined except dataset6 and dataset10"
+    #     boxplot_visual(data, tt, save_plots)
+    #     # print(f"Best Features for {tt}: {find_best_features(data)}")
+    #     # heatmap(data, tt, save_plots)
+
+
 
 
 
